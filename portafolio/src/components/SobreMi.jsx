022edd7a12baa3e81ed68331622bf37574a7ce74
NathyGaby04 <nathalie040904@gmail.com>
2026-04-09 08:06:26 -0500

    Actualización

--- portafolio/src/components/SobreMi.jsx
+++ portafolio/src/components/SobreMi.jsx
@@ -7,8 +7,8 @@ function SobreMi() {
                 <p>Hola!, soy</p>
                 <h1>Nathalie Miranda</h1>
                 <h2>Tecnologa en Desarrollo de Software</h2>
-                <p>Apasionada por crear experiencias digitales impactantes. 
-                    Combino mi experiencia en análisis de datos, desarrollo Front-End y diseño UX/UI con sólidas habilidades blandas como comunicación asertiva,
+                <p>Apasionada por crear experiencias digitales, 
+                    combinando mi experiencia en análisis de datos, desarrollo Front-End y diseño UX/UI con sólidas habilidades blandas como comunicación asertiva,
                     liderazgo y trabajo en equipo. Me caracterizo por mi mentalidad proactiva para resolver problemas y mi capacidad para adaptarme rápidamente 
                     a nuevos entornos tecnológicos, siempre enfocada en la gestión eficiente del tiempo y la entrega de resultados de calidad.</p>
             </div>
