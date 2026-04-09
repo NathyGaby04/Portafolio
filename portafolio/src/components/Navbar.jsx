277726a7cd29ed6c0b00f3e33c5769a4ffaf0067
NathyGaby04 <nathalie040904@gmail.com>
2026-04-09 09:05:04 -0500

    Actualización

--- portafolio/src/components/Navbar.jsx
+++ portafolio/src/components/Navbar.jsx
@@ -1,16 +1,19 @@
-import logo from '../assets/MiLogo.png'
+import logo from '../assets/MiLogo.png';
 
 function Navbar() {
     return (
         <nav className="Navbar">
-        <img src={logo} alt="logo" />
-        <ul className="Nav-Links">
-            <li><a href="#SobreMi">Sobre Mi</a></li>
-            <li><a href="#Habilidades">Habilidades</a></li>
-            <li><a href="#Proyectos">Proyectos</a></li>
-            <li><a href="#Contacto">Contacto</a></li>
-        </ul>
+            <a href="/">
+                <img src={logo} alt="logo" />
+            </a>
+            <ul className="Nav-Links">
+                <li><a href="#SobreMi">Sobre Mi</a></li>
+                <li><a href="#Habilidades">Habilidades</a></li>
+                <li><a href="#Proyectos">Proyectos</a></li>
+                <li><a href="#Contacto">Contacto</a></li>
+            </ul>
         </nav>
-    )}
+    );
+}
 
-export default Navbar
+export default Navbar;
